6277338b9df76b93a62dd880b5f8ba6f5c3fa797
rohit-raj25 <rohitraj250310@gmail.com>
2024-08-04 21:54:11 +0530

    adding navbar

---
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -0,0 +1,11 @@
+import { SignIn } from "@clerk/clerk-react"
+
+const SignInPage = () => {
+  return (
+    <main className="auth-page">
+        <SignIn />
+    </main>
+  )
+}
+
+export default SignInPage
